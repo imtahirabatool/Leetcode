8340a4d6cfaa31eb05dd2d649fe0fc23461746df
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-06-29 17:23:39 +0500

    Time: 82 ms (87.01%) | Memory: 60.7 MB (98.54%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int n=nums.size(), i=0, j=1;
+
+        while(j<n){
+            if(nums[i]==nums[j]){
+                return true;
+            }
+            i++;
+            j++;
+        }
+        return false;
+    }
+};
