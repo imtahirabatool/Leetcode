23e782c141f87a11df56fa0369e392014cf1d2af
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-07-09 21:59:56 +0500

    Time: 32 ms (6.72%) | Memory: 27.8 MB (6.86%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        unordered_map<int, bool> map;
+        int n = nums.size();
+
+        for (int i : nums) {
+            map[i] = true;
+        }
+
+        for (int i = 0; i <= n; ++i) {  
+            if (map.find(i) == map.end()) {
+                return i;  // Return the missing number
+            }
+        }
+
+        return -1;
+    }
+};
