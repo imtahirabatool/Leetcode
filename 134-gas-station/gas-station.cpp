2492490a0081964e1963b326efc489bcc20675f3
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-07-20 22:11:00 +0500

    Time: 111 ms (26.43%) | Memory: 130.9 MB (11.01%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int totalDiff=0, fuel=0, index=0;
+        int n=gas.size();
+
+        for(int i=0; i<n; i++){
+            int diff=gas[i]-cost[i];
+            totalDiff+=diff;
+            fuel+=diff;
+            if(fuel<0){
+                index=i+1;
+                fuel=0;
+            }
+        }
+        return (totalDiff<0)?-1:index;
+    }
+};
