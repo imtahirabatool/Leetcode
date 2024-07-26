1f1817e6bd8daa103f50976efba509794ead00c9
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-07-26 15:21:47 +0500

    Time: 46 ms (14.51%) | Memory: 19.9 MB (13.02%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back({edge[1], edge[2]});
+            adj[edge[1]].push_back({edge[0], edge[2]});
+        }
+
+        int min_count = INT_MAX, ans_city;
+        for (int i = 0; i < n; i++) {
+            vector<int> dist(n, INT_MAX);
+            dist[i] = 0;
+            priority_queue<pair<int, int>, vector<pair<int, int>>,
+                           greater<pair<int, int>>>
+                pq;
+            pq.push({0, i});
+
+            while (!pq.empty()) {
+                auto [wt, node] = pq.top();
+                pq.pop();
+
+                for (const auto& it : adj[node]) {
+                    auto [adjNode, edgeWt] = it;
+
+                    if (dist[adjNode] > wt + edgeWt) {
+                        dist[adjNode] = wt + edgeWt;
+                        pq.push({dist[adjNode], adjNode});
+                    }
+                }
+            }
+
+            int count = 0;
+            for (int d : dist)
+                if (d <= distanceThreshold)
+                    ++count;
+
+            if (count <= min_count) {
+                min_count = count;
+                ans_city = i;
+            }
+        }
+
+        return ans_city;
+    }
+};
