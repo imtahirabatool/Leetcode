ea61d31870705128f303cf41a22bdb3b81e6b5bd
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-07-29 21:36:05 +0500

    Time: 2 ms (47.16%) | Memory: 7.9 MB (5.82%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        double angle = std::abs(30.0 * hour + 0.5 * minutes - 6.0 * minutes);
-        return angle <= 180.0 ? angle : 360.0 - angle;
+        double angle = abs(30 * hour + 0.5 * minutes - 6 * minutes);
+
+        return angle < 180 ? angle : 360 - angle;
     }
 };
