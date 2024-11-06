a04c313faa895ef780469b58354a03377d2dfa55
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-11-06 12:55:13 +0500

    Time: 23 ms (9.35%) | Memory: 34.4 MB (7.98%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        vector<int> setBits;
+        map<int, int> map;
+        for (auto it : nums) {
+            int count = 0;
+            for (int i = 0; i < 31; i++) {
+                if ((it & 1 << i))
+                    count++;
+            }
+            map[it] = count;
+        }
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] > nums[j]) {
+                    if (map[nums[i]] != map[nums[j]])
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+};
