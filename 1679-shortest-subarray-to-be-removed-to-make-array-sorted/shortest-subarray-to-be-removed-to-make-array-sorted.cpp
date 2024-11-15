89a792393a48a063f38bc12109d2745746d1e819
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-11-15 16:34:04 +0500

    Time: 0 ms (100.00%) | Memory: 69.7 MB (25.81%) - LeetSync

--- 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
+++ 1679-shortest-subarray-to-be-removed-to-make-array-sorted/shortest-subarray-to-be-removed-to-make-array-sorted.cpp
@@ -5,17 +5,20 @@ public:
         int n = arr.size();
         int left = 0, right = n - 1;
 
-        while (left + 1 < n && arr[left] <= arr[left + 1]) ++left;
+        while (left + 1 < n && arr[left] <= arr[left + 1])
+            ++left;
 
-        if (left == n - 1) return 0;
+        if (left == n - 1)
+            return 0;
 
-        while (right > 0 && arr[right - 1] <= arr[right]) --right;
+        while (right > 0 && arr[right - 1] <= arr[right])
+            --right;
 
         int minLength = min(n - left - 1, right);
         int i = 0, j = right;
         while (i <= left && j < n) {
             if (arr[i] <= arr[j]) {
-             minLength = min(minLength, j - i - 1);
+                minLength = min(minLength, j - i - 1);
                 ++i;
             } else {
                 ++j;
