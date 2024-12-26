942b3064a9461a9db1e13ccf092a8a475ba8745b
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2024-12-26 19:41:40 +0500

    Time: 0 ms (100.00%) | Memory: 12.5 MB (24.37%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int>res; //array initize
+        vector<int>res; 
         int sum=0;
         for(int i=0; i<nums.size(); i++){
             sum=sum+nums[i]; //sum=0===> 0+1=1, 1+2==3 3+3=6 6+4=10
