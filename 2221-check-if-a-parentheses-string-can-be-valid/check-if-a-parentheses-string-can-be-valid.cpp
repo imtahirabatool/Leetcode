69b0387e9de672bb6928e8e963d321c35a8cdfd4
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-01-12 15:18:20 +0500

    Time: 13 ms (59.39%) | Memory: 30.1 MB (70.56%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        if (s.length() % 2 != 0) return false; 
+        
+        int open = 0, close = 0;
+
+        // Forward pass
+        for (int i = 0; i < s.length(); ++i) {
+            if (locked[i] == '0' || s[i] == '(') open++;
+            else close++;
+            
+            if (close > open) return false;
+        }
+
+        open = 0, close = 0;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            if (locked[i] == '0' || s[i] == ')') close++;
+            else open++;
+            
+            if (open > close) return false;
+        }
+
+        return true;
+    }
+};
