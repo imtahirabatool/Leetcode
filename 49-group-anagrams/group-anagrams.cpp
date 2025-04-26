e4fa02add3a44cc4fb238fff10b1a4157db9013b
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-04-26 17:40:12 +0500

    Time: 18 ms (52.97%) | Memory: 25 MB (67.00%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> anagramGroups;
+        unordered_map<string, vector<string>>anagramGroup;
 
-        int n = strs.size();
-        if (n == 0) {
-            return {};
-        }
+        int n=strs.size();
+        if(n==0)return {};
 
-        for (int i = 0; i < n; ++i) {
-            string sortedStr = strs[i];
+        for(int i=0; i<n; ++i){
+            string sortedStr=strs[i];
             sort(sortedStr.begin(), sortedStr.end());
 
-            anagramGroups[sortedStr].push_back(strs[i]);
+            anagramGroup[sortedStr].push_back(strs[i]);
         }
 
-        vector<vector<string>> result;
-        for (const auto& group : anagramGroups) {
-            result.push_back(group.second);
+        vector<vector<string>>res;
+        for(const auto& group:anagramGroup){
+            res.push_back(group.second);
         }
 
-        return result;
+        return res;
     }
 };
