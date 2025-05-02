fdce556046e01beb62c7e5f3602f42c61b5205c9
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-05-02 19:42:12 +0500

    Time: 0 ms (100.00%) | Memory: 31.3 MB (80.50%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -4,13 +4,9 @@ public:
         int left = 0, right = nums.size() - 1;
 
         while (left <= right) {
-            if (nums[left] == target) {
-                return left;
-            } else if (nums[right] == target) {
-                return right;
-            }
-            left++;
-            right--;
+        if (nums[left] == target) return left;
+        if (nums[right] == target) return right;
+        left++; right--;
         }
         return -1;
     }
