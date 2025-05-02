af703a0f75a63a7b58ebe0fb1acadc3068aa4ee7
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-05-02 19:41:37 +0500

    Time: 0 ms (100.00%) | Memory: 31.2 MB (98.54%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,37 +1,16 @@
-// class Solution {
-// public:
-//     int search(vector<int>& nums, int target) {
-//         int l=0, r=nums.size();
-
-//         while(l<=r){
-//             int mid=l+(r-l)/2;
-
-//             if(nums[mid]==target){
-//                 return mid;
-//             }else if(target<nums[mid]){
-//                 r=mid-1;
-//             }else{
-//                 l=mid+1;
-//             }
-//         }
-//         return -1;
-//     }
-// };
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int r=nums.size()-1, l=0;
-        while(l<=r){
-            int mid=(l+r)/2;
-            if(target==nums[mid]){
-                return mid;
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            if (nums[left] == target) {
+                return left;
+            } else if (nums[right] == target) {
+                return right;
             }
-            else if(nums[mid]>target){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
+            left++;
+            right--;
         }
         return -1;
     }
