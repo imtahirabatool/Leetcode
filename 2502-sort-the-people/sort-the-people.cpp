a8c289e4026f720a91c3cfcd4af44fb030424077
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-05-12 21:55:44 +0500

    Time: 16 ms (35.48%) | Memory: 28.6 MB (54.31%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        map<int, string> mao;
+        map<int, string> map;
         vector<string> res;
         for (int i = 0; i < size(names); i++)
-            mao[heights[i]] = names[i];
-        for (auto i = mao.rbegin(); i != mao.rend(); ++i)
+            map[heights[i]] = names[i];
+        for (auto i = map.rbegin(); i != map.rend(); ++i)
             res.push_back(i->second);
         return res;
     }
