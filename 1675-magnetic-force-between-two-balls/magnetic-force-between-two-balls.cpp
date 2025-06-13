c3d4871d8cf946397f91ed8bd3b5e0194df4afa1
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-06-13 22:54:10 +0500

    Time: 37 ms (97.30%) | Memory: 61.8 MB (35.39%) - LeetSync

--- 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
+++ 1675-magnetic-force-between-two-balls/magnetic-force-between-two-balls.cpp
@@ -2,29 +2,30 @@ class Solution {
 public:
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(), position.end());
-        int l=1, h=(position.back()- position[0])/(m-1),ans=1;
+        int l = 1, h = (position.back() - position[0]) / (m - 1), ans = 1;
 
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(canWePlace(position, mid, m)){
-                ans=mid;
-                l=mid+1;
-            }else{
-                h=mid-1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (canWePlace(position, mid, m)) {
+                ans = mid;
+                l = mid + 1;
+            } else {
+                h = mid - 1;
             }
         }
         return ans;
     }
-    private:
-    bool canWePlace(const vector<int>&arr, int dist, int balls){
-        int countBalls=1;
-        int lastPlaced=arr[0];
-        for(int i=1; i<arr.size(); i++){
-            if(arr[i]-lastPlaced>=dist){
+
+private:
+    bool canWePlace(const vector<int>& arr, int dist, int balls) {
+        int countBalls = 1;
+        int lastPlaced = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            if (arr[i] - lastPlaced >= dist) {
                 countBalls++;
-                lastPlaced=arr[i];
+                lastPlaced = arr[i];
             }
-            if(countBalls>=balls){
+            if (countBalls >= balls) {
                 return true;
             }
         }
