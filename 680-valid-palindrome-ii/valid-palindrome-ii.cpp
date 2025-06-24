dbfe9b80b32fcf0ec130e9a2e5cfac670a44f1b0
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-06-24 21:46:59 +0500

    Time: 15 ms (5.04%) | Memory: 22.1 MB (81.12%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     bool validPalindrome(string s) {
         int left = 0, right = s.length() - 1;
-
         while (left <= right) {
             if (s[left] != s[right]) {
-                return isPalindrome(s, left + 1, right) ||
-                       isPalindrome(s, left, right - 1);
+                return isPalindrom(s, left + 1, right) ||
+                       isPalindrom(s, left, right - 1);
             }
             left++, right--;
         }
@@ -14,11 +13,11 @@ public:
     }
 
 private:
-    bool isPalindrome(string& s, int left, int right) {
-        while (left < right) {
-            if (s[left] != s[right])
+    bool isPalindrom(string& s, int left, int right) {
+        while (left <= right) {
+            if (s[left] != s[right]) {
                 return false;
-
+            }
             left++, right--;
         }
         return true;
