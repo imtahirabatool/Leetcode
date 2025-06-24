101f4a3dcfb199df83cf30319b85d3e1b26251ee
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-06-25 01:29:21 +0500

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.02%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -8,13 +8,11 @@ public:
             if (nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
                 low++, mid++;
-            }
-            else if (nums[mid] == 1) {
-                mid++;
-            }
-            else { // nums[mid] == 2
-                swap(nums[mid], nums[high]);
+            } else if (nums[mid] == 2) {
+                swap(nums[high], nums[mid]);
                 high--;
+            } else {
+                mid++;
             }
         }
     }
