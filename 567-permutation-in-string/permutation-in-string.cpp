f761c2c35c11ca1eaf6975e3147e4c13880e46e8
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-06-28 04:41:31 +0500

    Time: 1033 ms (6.58%) | Memory: 60.6 MB (8.41%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         int len1 = s1.length(), len2 = s2.length();
-        if (len1 > len2)
-            return false;
 
-        sort(s1.begin(), s1.end());
+        if (len1 > len2) return false;
 
-        for (int i = 0; i <= len2 - len1; ++i) {
-            string window = s2.substr(i, len1);
+        sort(s1.begin(), s1.end());// so that sep. perm na lenii paray
+
+        for (int i = 0; i <= len2 - len1; i++) {
+            string window = s2.substr(i, len1);// s1 kii length k substr ki window
+
             sort(window.begin(), window.end());
-            if (window == s1)
-                return true;
-        }
 
+            if (window == s1) return true;
+        }
         return false;
     }
 };
