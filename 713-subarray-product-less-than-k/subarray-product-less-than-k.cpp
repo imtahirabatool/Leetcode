ed7ada150609da1f503dce18c01e0463ab440ac0
Tahira Batool <138373725+imtahirabatool@users.noreply.github.com>
2025-08-07 12:54:22 +0500

    Time: 0 ms (100.00%) | Memory: 65.2 MB (66.44%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         if (k <= 1) return 0;
-        
-        int product = 1, count = 0;
-        int left = 0;
+        int count = 0, left = 0;
+        int product = 1;
 
         for (int right = 0; right < nums.size(); right++) {
             product *= nums[right];
@@ -13,11 +12,10 @@ public:
                 product /= nums[left];
                 left++;
             }
-
+            
             // all subarrays ending at right and starting from left to right
             count += (right - left + 1);
         }
-
         return count;
     }
 };
